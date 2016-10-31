c616891ea6936b560b4a28b2528ca2e0bdaa4fe6
brychter <brychter@despegar.com>
2016-10-30 22:51:47 -0300

    check if is empty images table

--- admin/getAllImages.php
+++ admin/getAllImages.php
@@ -4,15 +4,21 @@
     $connection = new connection;
 
 	if(!mysqli_connect_error()){
-		
+
 		$consulta = "SELECT * FROM images";
 		$response = mysqli_query($connection->connected,$consulta);
 
+		if (mysqli_num_rows($response) > 0) {
+			
+		
 		while($obj = mysqli_fetch_object($response)){
 			$matriz[] = array('name' => $obj->name, 'id_image' => $obj->id_image);
 		}
 		//, 'userID' => $obj->userID
 		$datos = json_encode($matriz);
 		echo $datos;
+		}else{
+			echo "es 0";
+		}
 	}
 ?>
